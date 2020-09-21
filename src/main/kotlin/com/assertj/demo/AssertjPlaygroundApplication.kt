298534032f41739af1aeb671d0750bfaaca92fcb
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-21 08:14:22 +0100

    first commit

---
+++ src/main/kotlin/com/assertj/demo/AssertjPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.assertj.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssertjPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<AssertjPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/com/assertj/demo/AssertjPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assertj.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssertjPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
